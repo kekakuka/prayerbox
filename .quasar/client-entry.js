e91b9ac0397bd19ce2fcb4a5a360885bd2870c41
kekakuka <39260972@qq.com>
2019-03-12 20:56:03 +1300

    quasar

---
+++ .quasar/client-entry.js
@@ -0,0 +1,89 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+import 'quasar-framework/dist/quasar.ie.polyfills.js'
+
+
+
+import 'quasar-extras/roboto-font/roboto-font.css'
+
+import 'quasar-extras/material-icons/material-icons.css'
+
+
+
+
+import 'quasar-app-styl'
+
+
+import '../../prayerbox/src/css/app.styl'
+
+
+import Vue from 'vue'
+import createApp from './app.js'
+
+
+
+
+import pI18n from '../../prayerbox/src/plugins/i18n'
+
+import pAxios from '../../prayerbox/src/plugins/axios'
+
+import pVuelidate from '../../prayerbox/src/plugins/vuelidate'
+
+import pVueidb from '../../prayerbox/src/plugins/vueidb'
+
+import pOthers from '../../prayerbox/src/plugins/others'
+
+
+
+
+
+
+
+
+
+
+
+Vue.config.devtools = true
+Vue.config.productionTip = false
+
+
+
+console.info('[Quasar] Running SPA with MAT theme.')
+
+
+
+const { app, store, router } = createApp()
+
+
+
+
+;[pI18n,pAxios,pVuelidate,pVueidb,pOthers].forEach(plugin => {
+    plugin({
+        app,
+        router,
+        store,
+        Vue,
+        ssrContext: null
+    })
+})
+
+
+
+
+
+
+
+
+
+new Vue(app)
+
+
+
---
+++ .quasar/import-quasar.js
@@ -0,0 +1,17 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QFab,QFabAction,QInput,QDatetime,QCard,QCardTitle,QCardMain,QCardMedia,QCardSeparator,QCardActions,CloseOverlay,Notify,LocalStorage,SessionStorage} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},components: {QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QFab,QFabAction,QInput,QDatetime,QCard,QCardTitle,QCardMain,QCardMedia,QCardSeparator,QCardActions},directives: {CloseOverlay},plugins: {Notify,LocalStorage,SessionStorage} })
